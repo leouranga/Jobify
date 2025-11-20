32e8aa039f597c2f6a8c7aa01deffea92d5d7aee
Leo Uranga <leo.uranga@hotmail.com>
2025-11-20 02:11:44 -0500

    Project done

--- client/src/pages/DeleteJob.jsx
+++ client/src/pages/DeleteJob.jsx
@@ -1,13 +1,17 @@
 import { redirect } from "react-router-dom";
 import customFetch from "../utils/customFetch";
 import { toast } from "react-toastify";
+import { QueryClient } from "@tanstack/react-query";
 
-export const action = async ({ params }) => {
-  try {
-    await customFetch.delete(`/jobs/${params.id}`);
-    toast.success("Job deleted successfully");
-  } catch (error) {
-    toast.error(error.response.data.msg);
-  }
-  return redirect("/dashboard/all-jobs");
-};
+export const action =
+  (QueryClient) =>
+  async ({ params }) => {
+    try {
+      await customFetch.delete(`/jobs/${params.id}`);
+      QueryClient.invalidateQueries(["jobs"]);
+      toast.success("Job deleted successfully");
+    } catch (error) {
+      toast.error(error.response.data.msg);
+    }
+    return redirect("/dashboard/all-jobs");
+  };
